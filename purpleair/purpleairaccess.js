b5e92b17d7ebeb1b0c4a720208b852f3210695ce
Justin Ennis <justin@nrc-llc.com>
2021-09-27 20:41:50 -0700

    Changing the references.

--- purpleair/purpleairaccess.js
+++ purpleair/purpleairaccess.js
@@ -21,7 +21,7 @@ function getAqi(sensorid) {
     fetch("https://api.purpleair.com/v1/sensors/"+sensorid, initObject)
     .then(function (response) {
         const sensorData = response.json();
-        document.getElementById("demo").innerHTML = String(sensorData.sensor["pm2.5"]);
+        document.getElementById("demo").innerHTML = String(sensorData.result.sensor["pm2.5"]);
     })
     .catch(function (err) {
         console.log("Something went wrong!", err);
