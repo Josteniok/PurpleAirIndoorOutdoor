696d18e3906c65a63a42f6afaef2fbcd498f8284
Justin Ennis <justin@nrc-llc.com>
2021-09-27 20:05:04 -0700

    First try at getting sensor data from PurpleAir.

--- purpleair/purpleairaccess.js
+++ purpleair/purpleairaccess.js
@@ -1,9 +1,26 @@
 'use strict';
 
-let myVar = setInterval(myTimer, 1000);
+// let myVar = setInterval(myTimer, 1000);
+const purpleAirApiReadKey = 'ADB7BE2F-17CD-11EC-BAD6-42010A800017';
+const outdoorsensorid = '121389';
+const indoorsensorid = '125241';
+let myVar = getAqi(outdoorsensorid);
 
 function myTimer() {
   let d = new Date();
   let t = d.toLocaleTimeString();
   document.getElementById("demo").innerHTML = t;
+}
+
+function getAqi(sensorid) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            const sensorData = JSON.parse(this.responseText);
+            document.getElementById("demo").innerHTML = sensorData;
+        }
+    }
+    xhttp.setRequestHeader('X-API-Key', purpleAirApiReadKey);
+    xhttp.open("GET", "https://api.purpleair.com/v1/sensors/"+sensorid, true);
+    xhttp.send();
 }
