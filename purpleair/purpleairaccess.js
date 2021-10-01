40e381f42132b17ae405ff861d46e2304922aceb
Justin Ennis <justin@nrc-llc.com>
2021-09-30 19:24:30 -0700

    Added AQI calculation logic and re-enabled both sensors.

--- purpleair/purpleairaccess.js
+++ purpleair/purpleairaccess.js
@@ -4,7 +4,7 @@ const purpleAirApiReadKey = "ADB7BE2F-17CD-11EC-BAD6-42010A800017";
 const outdoorsensorid = "121389";
 const indoorsensorid = "125241";
 let indoorAQI = setInterval(getAqi, 20000, indoorsensorid, 'indooraqi');
-// let outdoorAQI = setInterval(getAqi, 2000, outdoorsensorid, 'outdooraqi');
+let outdoorAQI = setInterval(getAqi, 2000, outdoorsensorid, 'outdooraqi');
 
 function getAqi(sensorid, docid) {
     let customHeader = new Headers();
@@ -15,9 +15,61 @@ function getAqi(sensorid, docid) {
     fetch("https://api.purpleair.com/v1/sensors/"+sensorid, initObject)
     .then(response => response.json())
     .then(function (sensorData) {
-        document.getElementById(docid).innerHTML = String(sensorData.sensor["pm2.5"]);
+        let pm25data = sensorData.sensor["pm2.5"];
+        document.getElementById(docid).innerHTML = String(calcAQI(pm25data));
     })
     .catch(function (err) {
         console.log("ERROR: ", err);
     });
+}
+
+function calcAQI(pm25) {
+    let bphi = 12.0;
+    let bplo = 0.0;
+    let aqhi = 50;
+    let aqlo = 0;
+    switch (true) {
+        case (pm25 <= 12.0):
+            break;
+        case (pm25 > 12.0 && pm25 <=35.4):
+            bphi = 35.4;
+            bplo = 12.1;
+            aqhi = 100;
+            aqlo = 51;
+            break;
+        case (pm25 > 35.4 && pm25 <=55.4):
+            bphi = 55.4;
+            bplo = 35.5;
+            aqhi = 150;
+            aqlo = 101;
+            break;
+        case (pm25 > 55.4 && pm25 <=150.4):
+            bphi = 150.4;
+            bplo = 55.5;
+            aqhi = 200;
+            aqlo = 151;
+            break;
+        case (pm25 > 150.4 && pm25 <= 250.4):
+            bphi = 250.4;
+            bplo = 150.5;
+            aqhi = 300;
+            aqlo = 201;
+            break;
+        case (pm25 > 250.4 && pm25 <= 350.4):
+            bphi = 350.4;
+            bplo = 250.5;
+            aqhi = 400;
+            aqlo = 301;
+            break;
+        case (pm25 > 350.4 && pm25 <= 500.4):
+            bphi = 500.4;
+            bplo = 350.5;
+            aqhi = 500;
+            aqlo = 401;
+            break;
+        default:
+            break;
+    }
+    let aqi = ((aqhi - aqlo)/(bphi - bplo))*(pm25_24hour - bplo) + aqlo;
+    return aqi;
 }
