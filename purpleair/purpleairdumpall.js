1a83136fb871602dff050b9e2e2ac94c8df445dc
Justin Ennis <justin@nrc-llc.com>
2021-10-01 20:24:45 -0700

    Trying this to see if the pretty print works better.

--- purpleair/purpleairdumpall.js
+++ purpleair/purpleairdumpall.js
@@ -16,7 +16,7 @@ function getDetails(sensorid, location) {
     fetch("https://api.purpleair.com/v1/sensors/"+sensorid, initObject)
     .then(response => response.json())
     .then(function (data) {
-        document.getElementById(docid).innerHTML = JSON.stringify(data, null, "<br>");
+        document.getElementById(docid).innerHTML = "<pre>"+JSON.stringify(data, null, 2) +"</pre>"
     })
     .catch(function (err) {
         console.log("ERROR: ", err);
