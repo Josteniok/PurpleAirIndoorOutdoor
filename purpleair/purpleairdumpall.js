9f425707ee795174f6e594ba92bd75ab71628b06
Justin Ennis <justin@nrc-llc.com>
2021-10-01 20:14:54 -0700

    Found the JSON.stringify function.

--- purpleair/purpleairdumpall.js
+++ purpleair/purpleairdumpall.js
@@ -16,7 +16,7 @@ function getDetails(sensorid, location) {
     fetch("https://api.purpleair.com/v1/sensors/"+sensorid, initObject)
     .then(response => response.json())
     .then(function (data) {
-        document.getElementById(docid).innerHTML = data;
+        document.getElementById(docid).innerHTML = JSON.stringify(data);
     })
     .catch(function (err) {
         console.log("ERROR: ", err);
